2758da02ecdeb5e214a7fc0fb7aa45401cd55d07
martinescobarr <48950819+martinescobarr@users.noreply.github.com>
2019-07-05 11:54:11 -0500

    Add files via upload

---
+++ S5C3/EscobarMartin_S5C3_ODEs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+double pos(double x, double t);
+
+double vel();
+
+int main(){
+    
+}
+
+double pos(double x, double t)
+{
+        return (-k/m)/x;
+}
+
+double vel()
+{
+        return v;
+}
+        
