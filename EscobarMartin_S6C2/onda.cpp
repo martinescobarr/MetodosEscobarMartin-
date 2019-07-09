1351f507071633cab753fc9474a92117fa3d2732
martinescobarr <m.escobar@uniandes.edu.co>
2019-07-09 15:33:47 +0000

    otro2

--- EscobarMartin_S6C2/onda.cpp
+++ EscobarMartin_S6C2/onda.cpp
@@ -1,1 +1,15 @@
-v
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    
+    int A = 2;
+    int nx = 80;
+    int v =1;
+    int i;
+    int j;
+    int delta_x = A/nx;
+    int delta_t = (delta_x/v)*(0,25);
+    int upre[nx];
+    int upos[nx];
